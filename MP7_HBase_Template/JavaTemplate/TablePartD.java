3ba387f3fd971a76b500e78d7e8b80735af899c5
yz96 <68858113+yangzho12138@users.noreply.github.com>
2023-02-26 00:53:34 -0600

    read data

--- MP7_HBase_Template/JavaTemplate/TablePartD.java
+++ MP7_HBase_Template/JavaTemplate/TablePartD.java
@@ -16,31 +16,43 @@ import org.apache.hadoop.hbase.client.ResultScanner;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.client.Get;
 
-
 import org.apache.hadoop.hbase.util.Bytes;
+
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Table;
 
 public class TablePartD{
 
    public static void main(String[] args) throws IOException {
+	Configuration config = HBaseConfiguration.create();
 
-	// TODO      
-	// DON' CHANGE THE 'System.out.println(xxx)' OUTPUT PART
-	// OR YOU WON'T RECEIVE POINTS FROM THE GRADER 
+    Connection connection = ConnectionFactory.createConnection(config);
+
+	Table table = connection.getTable(TableName.valueOf("powers"));
+
+	Get get = new Get(Bytes.toBytes("row1"));
+	Result result = table.get(get);
 	
-	String hero = ???;
-	String power = ???;
-	String name = ???;
-	String xp = ???;
-	String color = ???;
+	String hero = Bytes.toString(result.getValue(Bytes.toBytes("personal"), Bytes.toBytes("hero")));
+	String power = Bytes.toString(result.getValue(Bytes.toBytes("personal"), Bytes.toBytes("power")));
+	String name = Bytes.toString(result.getValue(Bytes.toBytes("professional"), Bytes.toBytes("name")));
+	String xp = Bytes.toString(result.getValue(Bytes.toBytes("professional"), Bytes.toBytes("xp")));
+	String color = Bytes.toString(result.getValue(Bytes.toBytes("custom"), Bytes.toBytes("color")));
 	System.out.println("hero: "+hero+", power: "+power+", name: "+name+", xp: "+xp+", color: "+color);
 
-	hero = ???;
-	color = ???;
+
+	get = new Get(Bytes.toBytes("row19"));
+	result = table.get(get);
+	hero = Bytes.toString(result.getValue(Bytes.toBytes("personal"), Bytes.toBytes("hero")));
+	color = Bytes.toString(result.getValue(Bytes.toBytes("custom"), Bytes.toBytes("color")));
 	System.out.println("hero: "+hero+", color: "+color);
 
-	hero = ???;
-	name = ???;
-	color = ???;
+	get = new Get(Bytes.toBytes("row1"));
+	result = table.get(get);
+	hero = Bytes.toString(result.getValue(Bytes.toBytes("personal"), Bytes.toBytes("hero")));
+	name = Bytes.toString(result.getValue(Bytes.toBytes("professional"), Bytes.toBytes("name")));
+	color = Bytes.toString(result.getValue(Bytes.toBytes("custom"), Bytes.toBytes("color")));
 	System.out.println("hero: "+hero+", name: "+name+", color: "+color); 
    }
 }
